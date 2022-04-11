2d42dce886599a49ce2fdb9df2c74874af177d52
Eric Moran <emoran0403@gmail.com>
2022-04-11 09:14:35 -0400

    server file done

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,23 @@
-import * as express from 'express';
-import apiRouter from './routes';
+import * as express from "express";
+import apiRouter from "./routes";
+import * as path from "path";
 
 const app = express();
 
-app.use(express.static('public'));
-app.use(apiRouter);
+app.use(express.static("public")); // send public directory to client
+app.use(express.json()); // allows for req.body parsing
+app.use(apiRouter); // sets up the routes for us
+
+const clientPaths = ["/", "blogs"]; // establishes paths for client to use
+
+// allows for refreshing on client paths
+app.use(clientPaths, (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
+
+// if the client tries to access /api/* they will be notified
+app.use("/api/*", (req, res) => res.sendStatus(404).json({ message: "Bad Route!" }));
+
+// redirects client back to home after attempting an incorrect route not handled above
+app.use("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
