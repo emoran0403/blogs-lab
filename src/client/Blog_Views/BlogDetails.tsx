32fa15805467d8fef7719b9368e5f617deee58d3
Eric Moran <emoran0403@gmail.com>
2022-05-05 10:55:10 -0400

    blog details now uses Fetcher

--- src/client/Blog_Views/BlogDetails.tsx
+++ src/client/Blog_Views/BlogDetails.tsx
@@ -3,13 +3,13 @@ import { Button } from "@mui/material";
 import * as Types from "../../types";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { useState } from "react";
+import Fetcher from "../Client_Utils/Fetcher";
 
 const BlogDetails = () => {
   const [title, setTitle] = useState<string>("");
   const [content, setContent] = useState<string>("");
   const [isEditing, setIsEditing] = useState<boolean>(false);
 
-  //! how to set isEditing back to false when user leaves this page?
   const { id } = useParams(); // we just need the id from the useParams object, so we destructure it
 
   const nav = useNavigate();
@@ -17,54 +17,75 @@ const BlogDetails = () => {
 
   const BLOG = loc.state as Types.Blog;
 
+  //! this needs to grab the author id from the token
   const updateBlog = () => {
-    fetch(`/api/blogs/${id}`, {
-      // use the route:  /api/chirps/ ...
-      method: "PUT", // ...send a PUT request...
-      headers: {
-        // ...specifying the type of content...
-        "content-type": "application/json",
-      },
-      //! grab the author id from jwt once i know how
-      body: JSON.stringify({ title, content, authorid: 25 }), // ...and deliver the content}
-    })
-      .then((res) => {
-        // then with that response
-        res.json().then((data) => {
-          // parse the response, then with the response
-          if (res.ok) {
-            // if it was a good response
-            console.log(`Update Blog Successful!`);
-            nav("/blogs"); // nav to blogs view
-          } else {
-            // if it was a bad response
-            throw new Error(data.message);
-          }
-        });
+    Fetcher.PUT(`/api/blogs/${id}`, { title, content, authorid: 25 })
+      .then(() => {
+        console.log(`Update Blog Successful!`);
+        nav("/blogs"); // nav to blogs view if no errors
       })
       .catch((error) => {
         console.log(`Update Blog Error...\n`);
         console.error(error);
       });
+
+    // fetch(`/api/blogs/${id}`, {
+    //   // use the route:  /api/chirps/ ...
+    //   method: "PUT", // ...send a PUT request...
+    //   headers: {
+    //     // ...specifying the type of content...
+    //     "content-type": "application/json",
+    //   },
+    //   //! grab the author id from jwt once i know how
+    //   body: JSON.stringify({ title, content, authorid: 25 }), // ...and deliver the content}
+    // })
+    //   .then((res) => {
+    //     // then with that response
+    //     res.json().then((data) => {
+    //       // parse the response, then with the response
+    //       if (res.ok) {
+    //         // if it was a good response
+    //         console.log(`Update Blog Successful!`);
+    //         nav("/blogs"); // nav to blogs view
+    //       } else {
+    //         // if it was a bad response
+    //         throw new Error(data.message);
+    //       }
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.log(`Update Blog Error...\n`);
+    //     console.error(error);
+    //   });
   };
 
   const deleteBlog = () => {
-    // contact /api/blogs/:id with a DELETE request to delete the specified blog
-    fetch(`/api/blogs/${id}`, { method: "DELETE" })
-      .then((res) => {
-        res.json().then((data) => {
-          if (res.ok) {
-            console.log(`Delete Blog Successful!`);
-            nav("/blogs"); // nav to blogs view
-          } else {
-            throw new Error(data.message);
-          }
-        });
+    Fetcher.DELETE(`/api/blogs/${id}`)
+      .then(() => {
+        console.log(`Delete Blog Successful!`);
+        nav("/blogs"); // nav to blogs view if no errors
       })
       .catch((error) => {
         console.log(`Delete Blog Error...\n`);
         console.error(error);
       });
+
+    // contact /api/blogs/:id with a DELETE request to delete the specified blog
+    // fetch(`/api/blogs/${id}`, { method: "DELETE" })
+    //   .then((res) => {
+    //     res.json().then((data) => {
+    //       if (res.ok) {
+    //         console.log(`Delete Blog Successful!`);
+    //         nav("/blogs"); // nav to blogs view
+    //       } else {
+    //         throw new Error(data.message);
+    //       }
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     console.log(`Delete Blog Error...\n`);
+    //     console.error(error);
+    //   });
   };
 
   const stuckem = () => {
