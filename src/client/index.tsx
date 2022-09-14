22d9360270bd6446e88ca3448f1f370476107760
Eric Moran <emoran0403@gmail.com>
2022-09-14 16:27:30 -0400

    removing scss

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./scss/app";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "@stripe/stripe-js";
 
 render(
